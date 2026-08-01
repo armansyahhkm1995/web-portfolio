e6505f3cf33632378164b9fad8269f5cec50bf98
armansyahhkm1995 <armansyahhakim263@gmail.com>
2026-08-01 20:15:25 +0700

    Refactor solution section

--- app/solutions/page.tsx
+++ app/solutions/page.tsx
@@ -1,12 +1,15 @@
 import FinalCtaSection from "@/components/sections/solution/FinalCtaSection";
 import HeroSection from "@/components/sections/solution/HeroSection";
 import ProductSection from "@/components/sections/solution/ProductSection";
+import { Suspense } from "react";
 
 export default function SolutionsPage() {
   return (
     <>
       <HeroSection />
-      <ProductSection />
+      <Suspense fallback={null}>
+        <ProductSection />
+      </Suspense>
       <FinalCtaSection />
     </>
   );
