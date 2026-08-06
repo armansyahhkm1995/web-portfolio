42009f15a21da0405bb66fd48ea384ed8f327f4b
armansyahhkm1995 <armansyahhakim263@gmail.com>
2026-08-06 14:55:20 +0700

    modify google-sheet.ts 6 august 2026

--- lib/google-sheet.ts
+++ lib/google-sheet.ts
@@ -3,7 +3,10 @@ import { google } from "googleapis";
 const auth = new google.auth.GoogleAuth({
   credentials: {
     client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(
+      /^"(.*)"$/,
+      "$1",
+    ).replace(/\\n/g, "\n"),
   },
   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
 });
